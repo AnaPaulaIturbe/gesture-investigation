f0e56f8df94b003d5a8eb12c35bf7f50692e528d
AnaPaulaIturbe <112582690+AnaPaulaIturbe@users.noreply.github.com>
2022-10-07 06:58:02 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.LeftTriangle)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Butterfly)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Tortoise)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Happy)
+})
